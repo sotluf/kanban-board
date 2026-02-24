521d7b4172a4fff7682149f7d8ab57ea36829517
Oleksandra Maryanko <maryankoaleksandra@gmail.com>
2026-02-24 14:43:15 +0100

    feat: add edit and delete task functionality

--- js/script.js
+++ js/script.js
@@ -2,12 +2,37 @@ const modal = document.querySelector(".confirm-modal");
 const columnsContainer = document.querySelector(".columns");
 const columns = columnsContainer.querySelectorAll(".column");
 
+let currentTask = null;
+
 // function
+
+const handleDelete = (event) => {
+  currentTask = event.target.closest(".task");
+
+  // show preview
+  modal.querySelector(".preview").innerText = currentTask.innerText.substring(
+    0,
+    100,
+  );
+  modal.showModal();
+};
+
+const handleEdit = (event) => {
+  const task = event.target.closest(".task");
+  const input = createTaskInput(task.innerText);
+  task.replaceWith(input);
+  input.focus();
+
+  //move cursor to the end
+  const selection = window.getSelection();
+  selection.selectAllChildren(input);
+  selection.collapseToEnd();
+};
 
 const handleBlur = (event) => {
   const input = event.target;
-  const content = input.innerText.trim() || "Untitled";
-  const task = createTask(content.replace(/\n/g, "<br>"));
+  const content = input.innerHTML.trim() || "Untitled";
+  const task = createTask(content);
   input.replaceWith(task);
 };
 
@@ -41,10 +66,20 @@ const createTaskInput = (text = "") => {
   return input;
 };
 
-// task input
-
+// add, edit and delete task
 columnsContainer.addEventListener("click", (event) => {
   if (event.target.closest("button[data-add]")) {
     handleAdd(event);
+  } else if (event.target.closest("button[data-edit]")) {
+    handleEdit(event);
+  } else if (event.target.closest("button[data-delete]")) {
+    handleDelete(event);
   }
 });
+
+// confirm deletion
+modal.addEventListener("submit", () => currentTask && currentTask.remove());
+// cancel deletion
+modal.querySelector("#cancel").addEventListener("click", () => modal.close());
+// clear current task
+modal.addEventListener("close", () => (currentTask = null));
